a23cb24fb1fb94e7f1bf914957a29f9ab45fe022
bluyam <bluyam@tamu.edu>
2015-08-16 08:48:41 -0700

    added web interface for temperature

--- pebble_next_api/routes/index.js
+++ pebble_next_api/routes/index.js
@@ -2,11 +2,25 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
+// req - request details (parameters)
+// res - http response 
+// next - find next router
+router.get('/', function(req, res, next) {
+  console.log("Getting root");
+  res.render("index");
+});
+
+router.post('/', function(req, res) {
+    console.log("Temperature set to (F):");
+    console.log(req.body.temp);
+    res.send('Temperature submitted, now you may use your pebble!');
+}); 
+
 router.post('/nest_update_temp', function(req, res, next) {
   console.log("Nest is currently updating...");
   console.log("Switching to " + req.body.move + " mode!");
   var nest_url = "https://developer-api.nest.com/devices/thermostats/wbEQifUE9PfijiJ2mBfW7bfjfUcO-J0l?auth=c.AbFNpVV3PvzztntF6ttP7kEMDOe56XM6uiFh2ZO2XEqjeCrXwzzczdldQzhX6yAGMyw7tYA8nfqgRuLr03WlwlXFVPKmccTX1rA7e3A2i06HFSdbD6cIWf8EUobHMHHygbtTVyDS0PSYlR4L";
-  var new_temp = req.body.temp;
+  var new_temp = req.body.temp; // variable
   var temp, hvac;
   request({
     method: "GET", 
