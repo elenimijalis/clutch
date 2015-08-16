d9fc3bf4b05a9e352601ca154565666aa9b0ddbe
Eleni Mijalis <elenimijalis@gmail.com>
2015-08-15 18:50:06 -0700

    Fixed the shit out of it

--- Pebble/src/js/pebble-js-app.js
+++ Pebble/src/js/pebble-js-app.js
@@ -8,10 +8,7 @@ Pebble.addEventListener('appmessage', function(e) {
   console.log('PebbleKit JS Ready!');
   var request = new XMLHttpRequest();
   request.open("POST", "http://52.20.56.186/nest_update");
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  if (e.payload.SLEEP_KEY) {
-    request.send({ mode: "sleep" });
-  } else if (e.payload.RUN_KEY) {
-    request.send({ mode: "run" });
-  }
+  request.setRequestHeader('Content-Type', 'application/json');
+  var reqBody = (e.payload.SLEEP_KEY) ? { "mode": "sleep" } : { "mode": "run" };
+  request.send(JSON.stringify(reqBody));
 });
--- pebble_next_api/routes/index.js
+++ pebble_next_api/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post('/nest_update', function(req, res, next) {
   console.log("Nest is currently updating...");
   console.log("\nREQUEST:\n");
-  console.log(req.body.mode));
+  console.log(JSON.stringify(req.body));
 });
 
 module.exports = router;
