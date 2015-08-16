712698705f2a54eac8b9ffb44642e9d177f5e7bb
Eleni Mijalis <elenimijalis@gmail.com>
2015-08-15 18:38:03 -0700

    Updated server log

--- pebble_next_api/routes/index.js
+++ pebble_next_api/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post('/nest_update', function(req, res, next) {
   console.log("Nest is currently updating...");
   console.log("\nREQUEST:\n");
-  console.log(JSON.stringify(req.body));
+  console.log(req.body);
 });
 
 module.exports = router;
